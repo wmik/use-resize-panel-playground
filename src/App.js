31dc4ebaa6fded590877145fed67fbabae6042d6
wmik <williemik.wmik@gmail.com>
2020-06-27 08:15:57 +0300

    feat: user-defined configuration - Enable axis based configuration (x/y) - Enable size restrictions (minimum only) - Add multiple demos - Update styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import './styles.css';
 
-function useResizePanel() {
-  let resizableContainerRef = React.useRef(null);
+function useResizePanel(props) {
+  let config = Object.assign(
+    {
+      axis: 'x',
+      minSize: 0
+    },
+    props
+  );
   let resizableDragBarRef = React.useRef(null);
   let resizableRef = React.useRef(null);
   let [isDragging, setIsDragging] = React.useState(false);
+  let styleProperty = config.axis === 'y' ? 'height' : 'width';
+
+  React.useEffect(() => {
+    if (config.size) {
+      let isNumber = /\d$/.test(config.size);
+
+      // Inline styling takes precedence
+      // no need to break this model
+      if (resizableRef.current.style[styleProperty] === '') {
+        resizableRef.current.style[styleProperty] = `${config.size}${
+          isNumber ? 'px' : ''
+        }`.trim();
+      }
+    }
+    // Set the width only once if it is defined
+    // eslint-disable-next-line
+  }, []);
 
   React.useEffect(() => {
     function handleResize(e) {
-      let previousWidth = resizableRef.current.style.width.replace(/\D/g, '');
+      let sizeDelta = config.axis === 'y' ? e.movementY : e.movementX;
+      let previousSize = resizableRef.current.style[styleProperty].replace(
+        /\D/g,
+        ''
+      );
 
-      previousWidth = parseInt(previousWidth, 10);
+      previousSize = parseInt(previousSize, 10);
 
-      if (isDragging) {
-        resizableRef.current.style.width = `${previousWidth + e.movementX}px`;
+      let nextSize = previousSize + sizeDelta;
+
+      if (isDragging && nextSize >= config.minSize) {
+        resizableRef.current.style[styleProperty] = `${nextSize}px`;
       }
     }
 
@@ -29,65 +59,150 @@ function useResizePanel() {
       }
     }
 
-    resizableDragBarRef.current.addEventListener(
-      'mousedown',
-      handleResizeStart
-    );
+    let currentResizableDragBarRef = resizableDragBarRef.current;
+
+    currentResizableDragBarRef.addEventListener('mousedown', handleResizeStart);
     document.addEventListener('mousemove', handleResize);
     document.addEventListener('mouseup', handleResizeEnd);
 
     return () => {
+      currentResizableDragBarRef.removeEventListener(
+        'mousedown',
+        handleResizeStart
+      );
       document.removeEventListener('mousemove', handleResize);
       document.removeEventListener('mouseup', handleResizeEnd);
     };
-  }, [isDragging]);
+  }, [isDragging, config, styleProperty]);
 
   return {
-    resizableContainerRef,
     resizableDragBarRef,
-    resizableRef
+    resizableRef,
+    isDragging
   };
 }
 
-export default function App() {
-  let {
-    resizableContainerRef,
-    resizableDragBarRef,
-    resizableRef
-  } = useResizePanel();
+export function HorizontalResize() {
+  let { resizableDragBarRef, resizableRef, isDragging } = useResizePanel({
+    minSize: 200,
+    size: 400
+  });
 
   return (
-    <section
-      ref={resizableContainerRef}
-      style={{
-        display: 'flex',
-        height: 200
-      }}
-    >
-      <aside style={{ width: 300 }} ref={resizableRef}>
-        <ul>
-          <li>Item a</li>
-          <li>Item b</li>
-          <li>Item c</li>
-        </ul>
-      </aside>
-      <div
+    <React.Fragment>
+      <section
         style={{
-          width: 5,
-          backgroundColor: 'hsl(210, 83%, 67%)',
-          height: '100%',
-          cursor: 'col-resize'
+          display: 'flex',
+          height: 200
         }}
-        ref={resizableDragBarRef}
-      />
-      <article>
-        <h2>Heading</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ex
-          quod. Quae distinctio quod dignissimos fugiat hic nisi error ducimus!
-          Adipisci qui at eius dolorum libero reiciendis cum culpa quos!
-        </p>
-      </article>
-    </section>
+      >
+        <aside
+          style={{ width: 300, backgroundColor: 'hsl(210, 23%, 90%)' }}
+          ref={resizableRef}
+        >
+          <ul>
+            <li>Item a</li>
+            <li>Item b</li>
+            <li>Item c</li>
+          </ul>
+        </aside>
+        <div
+          className="resizable-drag-bar"
+          style={{
+            width: 5,
+            backgroundColor: 'hsl(210, 83%, 67%)',
+            height: '100%',
+            cursor: 'col-resize'
+          }}
+          ref={resizableDragBarRef}
+        />
+        <article style={{ padding: '0 8px' }}>
+          <h2>Heading</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ex
+            quod. Quae distinctio quod dignissimos fugiat hic nisi error
+            ducimus! Adipisci qui at eius dolorum libero reiciendis cum culpa
+            quos!
+          </p>
+        </article>
+      </section>
+      <pre
+        style={{
+          padding: 8,
+          backgroundColor: 'hsl(210, 20%, 96%)'
+        }}
+      >
+        <code>{JSON.stringify({ isDragging }, null, 2)}</code>
+      </pre>
+    </React.Fragment>
+  );
+}
+
+export function VerticalResizeApp() {
+  let { resizableDragBarRef, resizableRef, isDragging } = useResizePanel({
+    minSize: 100,
+    size: 150,
+    axis: 'y'
+  });
+
+  return (
+    <React.Fragment>
+      <section
+        style={{
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <aside
+          style={{ width: '100%', backgroundColor: 'hsl(210, 23%, 90%)' }}
+          ref={resizableRef}
+        >
+          <ul>
+            <li>Item a</li>
+            <li>Item b</li>
+            <li>Item c</li>
+          </ul>
+        </aside>
+        <div
+          className="resizable-drag-bar"
+          style={{
+            width: '100%',
+            backgroundColor: 'hsl(210, 83%, 67%)',
+            height: 5,
+            cursor: 'row-resize'
+          }}
+          ref={resizableDragBarRef}
+        />
+        <article style={{ padding: '0 8px' }}>
+          <h2>Heading</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ex
+            quod. Quae distinctio quod dignissimos fugiat hic nisi error
+            ducimus! Adipisci qui at eius dolorum libero reiciendis cum culpa
+            quos!
+          </p>
+        </article>
+      </section>
+      <pre
+        style={{
+          padding: 8,
+          backgroundColor: 'hsl(210, 20%, 96%)'
+        }}
+      >
+        <code>{JSON.stringify({ isDragging }, null, 2)}</code>
+      </pre>
+    </React.Fragment>
+  );
+}
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <h2>Horizontal resize</h2>
+      <HorizontalResize />
+      <br />
+      <h2>Vertical resize</h2>
+      <VerticalResizeApp />
+    </React.Fragment>
   );
 }
